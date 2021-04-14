bc57725f6f34c41556179a4b0a72c7d0ffa501b7
Joyt <zhouytforever@gmail.com>
2021-04-15 00:08:47 +0800

    decreasingArray 有一个巨大的坑

--- src/greed/decreasing-array/decreasing-array.js
+++ src/greed/decreasing-array/decreasing-array.js
@@ -1,9 +1,14 @@
-export const decreasingArray = input => {
+export const decreasingArray = nums => {
   let dcrsParts = 0
-  for (let i = 1, len = input.length; i < len; i++) {
-    if (input[i] < input[i - 1]) {
+  for (let i = 1, len = nums.length; i < len; i++) {
+    if (nums[i] < nums[i - 1]) {
       dcrsParts++
       if (dcrsParts > 1) return false
+      if (i === 1 || nums[i] >= nums[i - 2]) {
+        nums[i - 1] = nums[i]
+      } else {
+        nums[i] = nums[i - 1]
+      }
     }
   }
   return true
--- src/greed/decreasing-array/decreasing-array.test.js
+++ src/greed/decreasing-array/decreasing-array.test.js
@@ -10,4 +10,19 @@ describe('decreasing-array', () => {
     const result = decreasingArray(input)
     result.should.to.equal(false)
   })
+  it('case 3', () => {
+    const input = [3, 4, 2, 3]
+    const result = decreasingArray(input)
+    result.should.to.equal(false)
+  })
+  it('case 4', () => {
+    const input = [1, 4, 2, 5]
+    const result = decreasingArray(input)
+    result.should.to.equal(true)
+  })
+  it('case 5', () => {
+    const input = [3, 4, 2, 5]
+    const result = decreasingArray(input)
+    result.should.to.equal(true)
+  })
 })
