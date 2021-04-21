cbddbc4cb56dc03959015e973662a97bd4b551e9
Joyt <zhouytforever@gmail.com>
2021-04-21 16:06:58 +0800

    remove import chai from binarySearch.js

--- src/search/binarySearch.js
+++ src/search/binarySearch.js
@@ -1,10 +1,10 @@
 export const noop = () => {}
-export const binarySearch = (key, arr) => {
+export const binarySearch = (key, arr, fn = (key, arrMid) => key < arrMid) => {
   let lo = 0; let hi = arr.length - 1
   while (lo <= hi) {
     const mid = Number.parseInt(lo + (hi - lo) / 2)
-    if (key < arr[mid]) hi = mid - 1
-    else if (key > arr[mid]) lo = mid + 1
+    if (fn(key, arr[mid])) hi = mid - 1
+    else if (fn(arr[mid], key)) lo = mid + 1
     else return mid
   }
   return null
