4c2084fe77da7052229b374e7e6f5599b212d59e
Joyt <zhouytforever@gmail.com>
2021-04-22 03:42:36 +0800

    fix bug in palindrome
    1. fix bug
    2. change cmd 'ad' into 'db'

---
+++ src/pointer/palindrome-valid/debug.js
@@ -0,0 +1,7 @@
+import { palindromeValid } from './palindrome-valid.js'
+const run = () => {
+  const input = 'abcddeba'
+  const result = palindromeValid(input)
+  console.log(result)
+}
+run()
--- src/pointer/palindrome-valid/palindrome-valid.js
+++ src/pointer/palindrome-valid/palindrome-valid.js
@@ -2,7 +2,7 @@ const isPalinrome = s => {
   let l = 0
   let r = s.length - 1
   while (l <= r) {
-    if (!s[l++] === s[r--]) return false
+    if (s[l++] !== s[r--]) return false
   }
   return true
 }
@@ -10,7 +10,7 @@ export const palindromeValid = input => {
   let l = -1
   let r = input.length
   while (++l <= --r) {
-    if (!input[l] === input[r]) {
+    if (input[l] !== input[r]) {
       if (!isPalinrome(input.substring(l + 1, r + 1)) &&
       !isPalinrome(input.substring(l, r))) return false
     }
